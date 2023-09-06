c9dd225bae12b278a691e095d6f7c5ab3fec5553
moye12325 <gengkang12325@gmail.com>
2023-09-06 19:22:40 +0800

    Blog项目练习--工程创建

---
+++ java基础/code/VariableTest.java
@@ -0,0 +1,12 @@
+public class VariableTest {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2;
+        num2 = 5;
+        int num3 = num1 + num2;
+        System.out.println(num3);
+
+        double num4 = 5.11362;
+        System.out.println(num4);
+    }
+}
