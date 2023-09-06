34618e9675cf0468e494e21ebfc45516253a8cab
moye12325 <gengkang12325@gmail.com>
2023-09-06 19:26:40 +0800

    Blog项目练习--工程创建

---
+++ MoyeBlog/com.blog/src/main/java/com/moye/MoyeBlogApplication.java
@@ -0,0 +1,11 @@
+package com.moye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MoyeBlogApplication.class,args);
+    }
+}
